f14a2ae985fac319a2cbfb81135bb71a77a8da80
Subha Deep Mishra <subhadeepmishra100@gmail.com>
2025-07-26 01:31:19 +0530

    Buildings data fetching api done

--- src/main/java/com/royalpg/pgwebsite/controller/BuildingController.java
+++ src/main/java/com/royalpg/pgwebsite/controller/BuildingController.java
@@ -3,8 +3,11 @@ package com.royalpg.pgwebsite.controller;
 import com.royalpg.pgwebsite.entity.BuildingEntity;
 import com.royalpg.pgwebsite.service.BuildingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/buildings")
@@ -21,7 +24,16 @@ public class BuildingController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteBuilding(@PathVariable Long id) {
-        buildingService.deleteBuildingById(id);
-        return ResponseEntity.ok("Building with id " + id + " deleted successfully.");
+        try {
+            buildingService.deleteBuildingById(id);
+            return ResponseEntity.ok("Building with id " + id + " deleted successfully.");
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public List<BuildingEntity> getAllBuildings() {
+        return buildingService.getAllBuildings();
     }
 }
--- src/main/java/com/royalpg/pgwebsite/service/BuildingService.java
+++ src/main/java/com/royalpg/pgwebsite/service/BuildingService.java
@@ -5,6 +5,8 @@ import com.royalpg.pgwebsite.repository.BuildingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BuildingService {
 
@@ -21,8 +23,12 @@ public class BuildingService {
 
     public void deleteBuildingById(Long id) {
         if (!buildingRepository.existsById(id)) {
-            throw new RuntimeException("Building not found with id: " + id);
+            throw new RuntimeException("Building with ID " + id + " does not exist");
         }
         buildingRepository.deleteById(id);
+    }
+
+    public List<BuildingEntity> getAllBuildings() {
+        return buildingRepository.findAll();
     }
 }
